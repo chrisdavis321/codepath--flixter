c967c8c4fe4eea92a3854d2896ad3b8830b7204e
chrisdavis321 <cdavi114@bulldogs.aamu.edu>
2022-02-02 23:10:35 -0600

    Added flixter files to repository

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Christopher Davis on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+    }
+    
+}
+
