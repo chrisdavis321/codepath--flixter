7727ff3575cc62caaa38bed374b5a92b8fd942c0
chrisdavis321 <cdavi114@bulldogs.aamu.edu>
2022-02-09 21:46:41 -0600

    Added Part 2 of App

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Christopher Davis on 2/9/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
